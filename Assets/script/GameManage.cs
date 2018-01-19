7a93221549f45e38ba39dcb03f7b8d1c1e028f26
jyzHome <jyzgo@163.com>
2018-01-19 17:37:40 +1100

    remove unused level

--- Assets/script/GameManage.cs
+++ Assets/script/GameManage.cs
@@ -39,7 +39,7 @@ public enum PlayerStyle
 
 public class GameManage : MonoBehaviour
 {
-    public static int gameLevel = 5; // 游戏关卡
+    public static int gameLevel = 8; // 游戏关卡
 
     public static GameStatus gameStatus = GameStatus.Home; // 游戏状态
 
