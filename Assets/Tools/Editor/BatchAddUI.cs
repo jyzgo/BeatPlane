9fe8365b3988eab78f946d5e943777e3c8c53c29
jyzgoLyu <jyzgo@163.com>
2018-02-20 11:01:49 +0800

    add tool to add ui to monster

---
+++ Assets/Tools/Editor/BatchAddUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditPrefab
+{
+    [MenuItem("Tools/AddUI")]
+    private static void AddHitData()
+    {
+        if (EditorUtility.DisplayDialog("Confirm",
+            "Add monster hp",
+            "confirm"))
+        {
+            string genPath = Application.dataPath + "/Prefabs/Monster";
+            string[] filesPath = Directory.GetFiles(genPath, "*.prefab", SearchOption.AllDirectories);
+
+            for (int i = 0; i < filesPath.Length; i++)
+            {
+                Debug.Log("file " + filesPath[i]);
+                filesPath[i] = filesPath[i].Substring(filesPath[i].IndexOf("Assets"));
+
+              //  GameObject _prefab = AssetDatabase.LoadAssetAtPath(filesPath[i], typeof(GameObject)) as GameObject;
+                //GameObject prefabGameobject = PrefabUtility.InstantiatePrefab(_prefab) as GameObject;
+                //LeaderAction la = prefabGameobject.GetComponent<LeaderAction>();
+
+
+                //if (la != null)
+                //{
+                //    if (la.HIT_DATA != null) continue;
+                //    if (prefabGameobject.transform.FindChild("ANIMATION_DATA/HIT") == null) continue;
+                //    la.HIT_DATA = prefabGameobject.transform.FindChild("ANIMATION_DATA/HIT").GetComponent<AnimationData>();
+                //    PrefabUtility.ReplacePrefab(prefabGameobject, _prefab, ReplacePrefabOptions.Default);
+                //    MonoBehaviour.DestroyImmediate(prefabGameobject);
+                //}
+                //else
+                //    continue;
+
+            }
+            AssetDatabase.SaveAssets();
+            EditorUtility.DisplayDialog("success", "Add finish！", "Confirm");
+        }
+    }
+}
